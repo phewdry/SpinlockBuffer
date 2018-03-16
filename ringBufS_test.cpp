ab7d79f657603156ae22d53e4cad7a22550cad8b
ChanForPres <24968877+ChanForPres@users.noreply.github.com>
2018-03-16 16:37:31 -0700

    successful function split

--- ringBufS_test.cpp
+++ ringBufS_test.cpp
@@ -6,7 +6,37 @@
 #include <pthread.h>
 #include  "ringBufS.h"
 
-void thread_func() {
+void thread_func(ringBufS *my_ringBuffer) {
+
+  int my_status, i;
+  printf("\nfifo empty status = %d", ringBufS_empty (my_ringBuffer));
+
+  puts ("\nThe following is the FIFO FULL status");
+  for (i=0;i<RBUF_SIZE;i++)
+  {
+    ringBufS_put (my_ringBuffer, (unsigned char)i);
+    my_status = ringBufS_full(my_ringBuffer);
+    if (0==(i%16))
+    {
+      putchar ('\n');
+    }
+    printf("%2d ", my_status);
+  }
+
+  printf("\nfifo empty status = %d", ringBufS_empty (my_ringBuffer));
+  puts ("\nThe following is the contents of the FIFO");
+  for (i=0;i<RBUF_SIZE;i++)
+  {
+    int my_datum = ringBufS_get(my_ringBuffer);
+    if (0==(i%16))
+    {
+      putchar ('\n');
+    }
+    printf("%02X ",my_datum);
+  }
+  printf("\nfifo empty status = %d", ringBufS_empty (my_ringBuffer));
+
+  putchar ('\n');
 
 
 
@@ -15,38 +45,10 @@ void thread_func() {
 
 int main(int argc, char* argv[])
 {
-  int i;
   ringBufS my_ringBuffer;
-  int my_status;
   ringBufS_init (&my_ringBuffer);
 
-  printf("\nfifo empty status = %d", ringBufS_empty (&my_ringBuffer));
-
-  puts ("\nThe following is the FIFO FULL status");
-  for (i=0;i<RBUF_SIZE;i++)
-  {
-    ringBufS_put (&my_ringBuffer, (unsigned char)i);
-    my_status = ringBufS_full(&my_ringBuffer);
-    if (0==(i%16))
-    {
-      putchar ('\n');
-    }
-    printf("%2d ", my_status);
-  }
-
-  printf("\nfifo empty status = %d", ringBufS_empty (&my_ringBuffer));
-  puts ("\nThe following is the contents of the FIFO");
-  for (i=0;i<RBUF_SIZE;i++)
-  {
-    int my_datum = ringBufS_get(&my_ringBuffer);
-    if (0==(i%16))
-    {
-      putchar ('\n');
-    }
-    printf("%02X ",my_datum);
-  }
-  printf("\nfifo empty status = %d", ringBufS_empty (&my_ringBuffer));
-
-  putchar ('\n');
+  thread_func(&my_ringBuffer);
+  
   return 0;
 }
